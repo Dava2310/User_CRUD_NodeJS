9ee6dd896b19addbc69ad54ca412adb498ba7591
Dava2310 <dvetecourt23@gmail.com>
2024-07-13 16:10:27 -0400

    Edit and Delete Function

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -38,4 +38,52 @@ router.get('/list', async(req, res) => {
     }
 });
 
+router.get('/edit/:id', async(req, res) => {
+    try {
+
+        const {id} = req.params;
+        const [user] = await pool.query('SELECT * FROM users WHERE id =?', [id]);
+        const userEdit = user[0];
+
+        res.render('users/edit', {user: userEdit});
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+})
+
+router.post('/edit/:id', async(req, res) => {
+    try {
+        
+        // Separating the data
+        const {name, last_name, username, email} = req.body;
+        const {id} = req.params;
+
+        const updatedUser = {
+            name,
+            last_name,
+            username,
+            email
+        }
+
+        // Updating the user provided
+        await pool.query('UPDATE users SET ? WHERE id =?', [updatedUser, id]);
+        res.redirect('/list')
+        
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+
+})
+
+router.get('/delete/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        await pool.query('DELETE FROM users WHERE id =?', [id]);
+        res.redirect('/list')
+    } 
+    catch (err) {
+        res.status(500).json({message: err.message});
+    }
+})
+
 export default router;
